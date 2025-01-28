e534099b9e53380a9ca96474b8da1a5140c734af
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-01-29 00:49:45 +0300

    add EntityNotFoundException.java

---
+++ projects/NestedResources/src/main/java/app/exception/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package app.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
